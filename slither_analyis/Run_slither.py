e90d003e68dd96e9abff294700d0a7465b6ca3a6
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-10-29 02:41:49 +0700

    nothing

--- slither_analyis/Run_slither.py
+++ slither_analyis/Run_slither.py
@@ -1,30 +1,46 @@
 import os
-import subprocess
 import json
+import subprocess
 
-# Thay đổi đường dẫn này thành thư mục chứa các tệp .sol của bạn
-directory = r'C:\Users\20520\Blockchain-Smart-Contract-Security\get_source\dataset'
+# Đường dẫn đến thư mục chứa các file .sol
+folder_path = r'C:\Users\Admin\OneDrive\Documents\GitHub\Blockchain-Smart-Contract-Security\get_source\dataset'
 
-results = []
+# Tạo một dictionary để lưu kết quả
+results = {}
 
-for filename in os.listdir(directory):
+# Duyệt qua tất cả các file trong thư mục
+for filename in os.listdir(folder_path):
     if filename.endswith('.sol'):
-        # Lấy phiên bản solc từ tệp .sol
-        with open(os.path.join(directory, filename), 'r') as file:
-            for line in file:
-                if line.startswith('pragma solidity'):
-                    version = line.split('^')[1].split(';')[0].strip()
+        # Tạo đường dẫn đầy đủ đến file
+        file_path = os.path.join(folder_path, filename)
+        
+        # Lấy phiên bản Solidity từ file .sol
+        with open(file_path, 'r',encoding='utf-8') as f:
+            lines = f.readlines()
+        pragma_line = next((line for line in lines if line.startswith('pragma solidity')), None)
+        split_line = pragma_line.split('^')
+        if len(split_line) > 1:
+            version = split_line[1].strip('; \n')
+        else:
+            version = 'latest'
 
-                    # Chọn phiên bản solc phù hợp
-                    subprocess.run(['solc-select', 'use', version], shell=True)
+        
+        # Chọn phiên bản Solidity phù hợp
+        subprocess.run(['solc-select', 'use', version], capture_output=True, text=True)
+        
+        # Chạy lệnh slither và lấy kết quả
+        result = subprocess.run(['slither', file_path], capture_output=True, text=True)
+        
+        # Kiểm tra xem liệu Slither có hoạt động đúng hay không
+        if result.returncode != 0:
+            print(f"An error occurred while running slither on {file_path}.")
+            results[filename] = result.stderr
+        else:
+            # Lấy phần kết quả xuất ra sau khi chạy lệnh slither
+            output_lines = result.stdout.split('\n')
+            info_lines = [line for line in output_lines if line.startswith('INFO:')]
+            results[filename] = '\n'.join(info_lines)
 
-                    # Chạy slither cho tệp .sol
-                    result = subprocess.run("slither '.\$MLG.sol'", shell=True, capture_output=True, text=True)
-
-                    # Lưu kết quả vào danh sách
-                    results.append({'filename': filename, 'result': result.stdout})
-                    break
-
-# Lưu kết quả vào tệp JSON
-with open('results.json', 'w') as file:
-    json.dump(results, file)
+# Lưu kết quả vào file JSON
+with open('results.json', 'w') as f:
+    json.dump(results, f)
