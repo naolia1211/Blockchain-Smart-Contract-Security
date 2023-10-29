1460b03a3eb26c010b2a2d2c0ceb8af7bd038629
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-10-29 14:56:19 +0700

    update Run_slither
    cập nhật lệnh chạy slither tự động và sẽ lưu kết quả về file JSON

--- slither_analyis/Run_slither.py
+++ slither_analyis/Run_slither.py
@@ -1,46 +1,37 @@
 import os
-import json
 import subprocess
+from packaging import version
 
-# Đường dẫn đến thư mục chứa các file .sol
-folder_path = r'C:\Users\Admin\OneDrive\Documents\GitHub\Blockchain-Smart-Contract-Security\get_source\dataset'
+def run_slither():
+    source_directory = "C:\\Users\\Admin\\OneDrive\\Documents\\GitHub\\Blockchain-Smart-Contract-Security\\get_source\\dataset"
+    output_directory = "C:\\Users\\Admin\\OneDrive\\Documents\\GitHub\\Blockchain-Smart-Contract-Security\\slither_analyis\\extract_feature"
+    for filename in os.listdir(source_directory):
+        if filename.endswith(".sol"):
+            file_path = os.path.join(source_directory, filename)
+            
+            # Đọc nội dung tệp và tìm dòng chứa 'pragma solidity'
+            with open(file_path, 'r') as f:
+                lines = f.readlines()
+            pragma_lines = [line for line in lines if line.startswith('pragma solidity')]
+            
+            # Tách phiên bản Solidity từ dòng 'pragma solidity'
+            versions = []
+            for pragma_line in pragma_lines:
+                if '^=' in pragma_line:
+                    versions.append(pragma_line.split('^=')[1].strip('; \n'))
+                elif '^' in pragma_line:
+                    versions.append(pragma_line.split('^')[1].strip('; \n'))
+                elif '=' in pragma_line:
+                    versions.append(pragma_line.split('=')[1].strip('; \n'))
+            
+            # Chọn phiên bản lớn nhất
+            max_version = str(max(version.parse(v) for v in versions)) if versions else 'latest'
+            
+            # Sử dụng solc-select để chọn phiên bản Solidity phù hợp
+            subprocess.run(['solc-select', 'use', max_version])
+            
+            print(f"Running slither on {filename} with Solidity version {max_version}")
+            json_file = os.path.join(output_directory, f"{os.path.splitext(filename)[0]}.json")
+            subprocess.run(["slither", file_path, "--json", json_file])
 
-# Tạo một dictionary để lưu kết quả
-results = {}
-
-# Duyệt qua tất cả các file trong thư mục
-for filename in os.listdir(folder_path):
-    if filename.endswith('.sol'):
-        # Tạo đường dẫn đầy đủ đến file
-        file_path = os.path.join(folder_path, filename)
-        
-        # Lấy phiên bản Solidity từ file .sol
-        with open(file_path, 'r',encoding='utf-8') as f:
-            lines = f.readlines()
-        pragma_line = next((line for line in lines if line.startswith('pragma solidity')), None)
-        split_line = pragma_line.split('^')
-        if len(split_line) > 1:
-            version = split_line[1].strip('; \n')
-        else:
-            version = 'latest'
-
-        
-        # Chọn phiên bản Solidity phù hợp
-        subprocess.run(['solc-select', 'use', version], capture_output=True, text=True)
-        
-        # Chạy lệnh slither và lấy kết quả
-        result = subprocess.run(['slither', file_path], capture_output=True, text=True)
-        
-        # Kiểm tra xem liệu Slither có hoạt động đúng hay không
-        if result.returncode != 0:
-            print(f"An error occurred while running slither on {file_path}.")
-            results[filename] = result.stderr
-        else:
-            # Lấy phần kết quả xuất ra sau khi chạy lệnh slither
-            output_lines = result.stdout.split('\n')
-            info_lines = [line for line in output_lines if line.startswith('INFO:')]
-            results[filename] = '\n'.join(info_lines)
-
-# Lưu kết quả vào file JSON
-with open('results.json', 'w') as f:
-    json.dump(results, f)
+run_slither()
