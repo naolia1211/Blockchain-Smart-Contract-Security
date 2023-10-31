029b0c5393f87bcbc6f0dac18f0106bfe9c1fcb4
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2023-10-31 09:57:53 +0700

    huhuhuhu

--- slither_analyis/Run_slither.py
+++ slither_analyis/Run_slither.py
@@ -11,22 +11,30 @@ def run_slither():
             file_path = os.path.join(source_directory, filename)
             
             # Đọc nội dung tệp và tìm dòng chứa 'pragma solidity'
-            with open(file_path, 'r') as f:
+            with open(file_path, 'r',encoding='utf-8') as f:
                 lines = f.readlines()
             pragma_lines = [line for line in lines if line.startswith('pragma solidity')]
             
             # Tách phiên bản Solidity từ dòng 'pragma solidity'
             versions = []
-            for pragma_line in pragma_lines:
-                if '^=' in pragma_line:
-                    versions.append(pragma_line.split('^=')[1].strip('; \n'))
-                elif '^' in pragma_line:
-                    versions.append(pragma_line.split('^')[1].strip('; \n'))
-                elif '=' in pragma_line:
-                    versions.append(pragma_line.split('=')[1].strip('; \n'))
-            
-            # Chọn phiên bản lớn nhất
-            max_version = str(max(version.parse(v) for v in versions)) if versions else 'latest'
+            try:
+                for pragma_line in pragma_lines:
+                    if '^=' in pragma_line:
+                        versions.append(pragma_line.split('^=')[1].strip('; \n'))
+                    elif '^' in pragma_line:
+                        versions.append(pragma_line.split('^')[1].strip('; \n'))
+                    elif '=' in pragma_line:
+                        versions.append(pragma_line.split('=')[1].strip('; \n'))
+                    elif '>=' in pragma_line:
+                        versions.extend([v.strip('; \n') for v in pragma_line.split('>=')[1:]])
+                    elif '<' in pragma_line:
+                        versions.extend([v.strip('; \n') for v in pragma_line.split('<')[1:]])
+                
+                # Chọn phiên bản lớn nhất nhưng nhỏ hơn 0.9.0
+                max_version = str(max(version.parse(v) for v in versions if version.parse(v) < version.parse('0.9.0'))) if versions else 'latest'
+            except Exception as e:
+                print(f"Error parsing Solidity version for {filename}: {str(e)}. Skipping...")
+                continue
             
             # Sử dụng solc-select để chọn phiên bản Solidity phù hợp
             subprocess.run(['solc-select', 'use', max_version])
@@ -42,7 +50,7 @@ def run_slither():
             subprocess.run(["slither", file_path, "--json", json_file])
             
             # Đọc tệp JSON đầu ra của Slither
-            with open(json_file, 'r') as f:
+            with open(json_file, 'r',encoding='utf-8') as f:
                 data = json.load(f)
             
 run_slither()
