d98bd3564077182d086b9db55e87167ef5182544
g3LO5 <hao3052002@gmail.com>
2024-05-23 09:43:22 +0700

    Update ExtractFeature.py

--- RunPy/ExtractFeature.py
+++ RunPy/ExtractFeature.py
@@ -1,39 +1,51 @@
 import re
 import os
 from pymongo import MongoClient
+from nltk.tokenize import RegexpTokenizer
 
 # Setup MongoDB connection
 client = MongoClient('mongodb://localhost:27017/')
 db = client['Interaction_and_Contract_State_Vulnerabilities']
-collection = db['test']
+collection = db['test1']
 
 def preprocess_code(code):
-    # Standard preprocessing to clean up the source code
-    code = re.sub(re.compile(r"/\*.*?\*/", re.DOTALL), "", code)
-    code = re.sub(re.compile(r"//.*?$", re.MULTILINE), "", code)
-    code = re.sub(r'\t', '', code)
-    code = re.sub(r' {2,}', '', code)
-    code = re.sub(r' *\n', '', code)
-    code = re.sub(r'\n\s*\n', '', code)
+    # Loại bỏ comment nhiều dòng (/* ... */)
+    code = re.sub(r'/\*[\s\S]*?\*/', '', code)
+    
+    # Loại bỏ comment một dòng (// ...)
+    code = re.sub(r'//.*', '', code)
+    
+    # Loại bỏ các ký tự tab (\t)
+    code = code.replace('\t', '')
+    
+    # Loại bỏ khoảng trắng thừa (2 hoặc nhiều khoảng trắng liên tiếp)
+    code = re.sub(r'\s{2,}', ' ', code)
+    
+    # Loại bỏ khoảng trắng trước dấu * ở đầu dòng
+    code = re.sub(r'^\s*\*', '', code, flags=re.MULTILINE)
+    
+    # Loại bỏ các dòng chỉ chứa khoảng trắng
+    code = re.sub(r'^\s*$', '', code, flags=re.MULTILINE)
+    
     return code.strip()
 
 def check_for_reentrancy_issues(function_content):
  
-       #reentrancy
+   #    #reentrancy
+    # vulnerability_patterns = {
+    #     'external_calls': r'(?<!\w)\.(call|delegatecall|send|transfer|staticcall)\b',
+    #     'use_of_msg_value_and_sender': r'\b(msg\.value|msg\.sender)\b',
+    #     'recursive_calls': r'function\s+(\w+)\s*\(.*?\)\s*\{[\s\S]*?(?<=\{|;)\s*\b\1\b\s*\(.*?\)\s*;',
+    #     'state_changes_after_external_calls': r'\.(call|delegatecall|send|transfer)\(.*?\)\s*;[\s\S]*?\s*\w+\s*=',
+    # }
+    #uncheck external call
     vulnerability_patterns = {
-        'external_calls': r'(?<!\w)\.(call|delegatecall|send|transfer|staticcall)\b',
-        'use_of_msg_value_and_sender': r'\b(msg\.value|msg\.sender)\b',
-        'recursive_calls': r'function\s+(\w+)\s*\(.*?\)\s*\{[\s\S]*?(?<=\{|;)\s*\b\1\b\s*\(.*?\)\s*;',
-        'state_changes_after_external_calls': r'\.(call|delegatecall|send|transfer)\(.*?\)\s*;[\s\S]*?\s*\w+\s*=',
-    }
-#     #uncheck external call
-#     vulnerability_patterns = {
-#         'external_calls': r'(?<!\w)(?:address\s*\(\s*\w+\s*\)\s*\.)?(?:send|call|delegatecall)\s*\(',
-#         'ignored_return_values': r'(?<!\w)(?:address\s*\(\s*\w+\s*\)\s*\.)?(?:send|call|delegatecall)\s*\(.*?\)\s*;(?!\s*(?:require|assert|revert)\s*\()',
-#         'state_changes_after_external_calls': r'(?<!\w)(?:address\s*\(\s*\w+\s*\)\s*\.)?(?:send|call|delegatecall)\s*\(.*?\)\s*;[\s\S]*?\s*\w+\s*=',
-#         'lack_of_error_handling': r'(?<!\w)(?:address\s*\(\s*\w+\s*\)\s*\.)?(?:send|call|delegatecall)\s*\(.*?\)\s*;(?!\s*(?:require|assert|revert)\s*\()',
-#         #'fallback_functions_without_gas_check': r'function\s+\(\)\s+(?:external\s+)?payable\s*\{(?![\s\S]*?require\s*\(\s*msg\.gas\s*>=\s*2300\s*\))'
-# }
+        'external_calls': r'(?<!\w)(?:address\s*\(\s*\w+\s*\)\s*\.)?(?:send|call|delegatecall)\s*\(',
+        'ignored_return_values': r'(?<!\w)(?:address\s*\(\s*\w+\s*\)\s*\.)?(?:send|call|delegatecall)\s*\(.*?\)\s*;(?!\s*(?:require|assert|revert)\s*\()',
+        'state_changes_after_external_calls': r'(?<!\w)(?:address\s*\(\s*\w+\s*\)\s*\.)?(?:send|call|delegatecall)\s*\(.*?\)\s*;[\s\S]*?\s*\w+\s*=',
+        'lack_of_error_handling': r'(?<!\w)(?:address\s*\(\s*\w+\s*\)\s*\.)?(?:send|call|delegatecall)\s*\(.*?\)\s*;(?!\s*(?:require|assert|revert)\s*\()',
+        #'fallback_functions_without_gas_check': r'function\s+\(\)\s+(?:external\s+)?payable\s*\{(?![\s\S]*?require\s*\(\s*msg\.gas\s*>=\s*2300\s*\))'
+}
 #     #delegatecall
 #     vulnerability_patterns = {
 #     'usage_of_delegatecall': r'(?<!\w)delegatecall\s*\(',
@@ -65,7 +77,6 @@ def check_for_reentrancy_issues(function_content):
     # 'usage_in_comparison_operations': r'block\.number\s*(==|!=|>|<|>=|<=)\s*<number>',
     # 'random_number_generation': r'(block\.blockhash\(block\.number\)|block\.number)'
     # }
-
     issues = []
     for issue, pattern in vulnerability_patterns.items():
         if re.search(pattern, function_content, re.MULTILINE | re.DOTALL):
@@ -74,41 +85,28 @@ def check_for_reentrancy_issues(function_content):
 
 def extract_functions_with_issues(content):
     functions_with_issues = []
-    function_block = []
-    brace_count = 0
-    inside_function = False
+    function_pattern = re.compile(r'function\s+(\w+)\s*\(.*?\)\s*\{(.*?)\}', re.DOTALL)
 
-    lines = content.split('\n')
-    for line in lines:
-        line = line.strip()
-        if 'function' in line and '{' in line:  # Function starts
-            inside_function = True
-            brace_count = 1
-            function_block = [line]
-        elif inside_function:
-            function_block.append(line)
-            if '{' in line:
-                brace_count += line.count('{')
-            if '}' in line:
-                brace_count -= line.count('}')
-            if brace_count == 0:  # Function ends
-                inside_function = False
-                function_text = '\n'.join(function_block)
-                function_name_match = re.search(r'function\s+([\w]+)', function_text)
-                if function_name_match:
-                    function_name = function_name_match.group(1)
-                    issues = check_for_reentrancy_issues(function_text)
-                    if issues:
-                        for issue in issues:
-                            functions_with_issues.append({
-                                "feature_type": issue,
-                                "function_name": function_name,
-                                "function_content": function_text
-                            })
-                function_block = []
+    for match in function_pattern.finditer(content):
+        function_name = match.group(1)
+        function_content = match.group(2).strip()
+
+        issues = check_for_reentrancy_issues(function_content)
+        if issues:
+            # Tokenize function_content
+            tokenizer = RegexpTokenizer(r'\w+|\{|\}|\(|\)|\\[|\\]|\.|\;|\=|\+|\-|\*|\/|\!|\%|<|>|\||\&')
+            tokens = tokenizer.tokenize(function_content)
+
+            # Modify here to get the desired output format
+            for issue in issues:
+                functions_with_issues.append({
+                    "feature_type": issue,
+                    "function_name": function_name,
+                    "function_content": function_content,
+                    "tokens": tokens
+                })
 
     return functions_with_issues
-
 
 def process_directory(directory_path):
     for filename in os.listdir(directory_path):
@@ -128,7 +126,7 @@ def process_directory(directory_path):
                 print(f"Processed and stored findings for {filename} in MongoDB.")
 
 # Specify the directory containing Solidity files
-solidity_files_directory = r'D:\GitHub\Blockchain-Smart-Contract-Security\Dataset\unchecked send'
+solidity_files_directory = r'C:\Users\hao30\Documents\GitHub\Blockchain-Smart-Contract-Security\Dataset\unchecked external call (UC)\source'
 
 # Process the directory
 process_directory(solidity_files_directory)
