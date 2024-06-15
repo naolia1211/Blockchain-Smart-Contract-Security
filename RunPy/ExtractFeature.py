081f1d92695ff3776f36bff54d48df7d82122a28
g3LO5 <75740255+g3LO5@users.noreply.github.com>
2024-06-15 13:40:00 +0700

    Update ExtractFeature.py

--- RunPy/ExtractFeature.py
+++ RunPy/ExtractFeature.py
@@ -13,7 +13,7 @@ current_dir = Path(__file__).resolve().parent
 solidity_files_directory = current_dir / "../Data/Interaction and Contract State Vulnerabilities/unchecked send"
 
 def check_for_reentrancy_issues(function_content):
-    #delegatecall
+    # delegatecall
     # vulnerability_patterns = {
     #     'usage_of_delegatecall': r'\bdelegatecall\s*\(',
     #     'state_variables_manipulation': r'\bdelegatecall\s*\([^)]*\)[^;]*;[^;]*\b\w+\s*=',
@@ -21,7 +21,7 @@ def check_for_reentrancy_issues(function_content):
     #     'context_preservation': r'\bdelegatecall\s*\([^)]*\)\s*;\s*(?!.*(?:require|assert)\s*\((?:this|msg\.sender)\s*==\s*)',
     #     'library_safe_practices': r'\blibrary\s+\w+\s*{[^{}]*?\bdelegatecall\b[^{}]*}'
     # }
-    #reentrancy
+    # reentrancy
     # vulnerability_patterns = {
     #     'external_calls': r'(?<!\w)\.(call|delegatecall|send|transfer|staticcall)\b',
     #     'use_of_msg_value_and_sender': r'\b(msg\.value|msg\.sender)\b',
@@ -36,29 +36,29 @@ def check_for_reentrancy_issues(function_content):
     #     'lack_of_error_handling': r'(?<!\w)(?:address\s*\(\s*\w+\s*\)\s*\.)?(?:send|call|delegatecall)\s*\(.*?\)\s*;(?!\s*(?:require|assert|revert)\s*\()',
     #     'fallback_functions_without_gas_check': r'function\s+\(\)\s+(?:external\s+)?payable\s*\{(?![\s\S]*?require\s*\(\s*msg\.gas\s*>=\s*2300\s*\))'
     # }
-    
-    # #unchecked send
-    vulnerability_patterns = {
-    'usage_of_send': r'(?<!\\)\bsend\s*\(',
-    'unchecked_send_return': r'\bsend\s*\(.*?\)\s*;(?!\s*(?:require|assert|revert|if)\s*\(.*?\)\s*;)',
-    'state_update_without_verification': r'\bsend\s*\(.*?\)\s*;[\s\S]*?\s*(?:\w+\s*=|(?:require|assert|revert|if)\s*\()',
-    'fallback_function_risks': r'function\s+\(\)\s+(?:external\s+)?payable\s*\{(?!.*(?:require|assert|revert)\s*\(msg\.value\s*(?:<=|<)\s*2300\))[\s\S]*?\bsend\b'
-    }
-    # #timestamp dependency
+    # # #unchecked send
     # vulnerability_patterns = {
-    # 'reliance_on_block_timestamp': r'block\.timestamp',
-    # 'miner_manipulation_risks': r'block\.timestamp|block\.number',
-    # 'incorrect_time_estimation': r'block\.number\s*(?:\*|\/)\s*\d+',
-    # 'timestamp_used_for_randomness': r'block\.timestamp.*%(?:\s*\d+)'
+    # 'usage_of_send': r'(?<!\\)\bsend\s*\(',
+    # 'unchecked_send_return': r'\bsend\s*\(.*?\)\s*;(?!\s*(?:require|assert|revert|if)\s*\(.*?\)\s*;)',
+    # 'state_update_without_verification': r'\bsend\s*\(.*?\)\s*;[\s\S]*?\s*(?:\w+\s*=|(?:require|assert|revert|if)\s*\()',
+    # 'fallback_function_risks': r'function\s+\(\)\s+(?:external\s+)?payable\s*\{(?!.*(?:require|assert|revert)\s*\(msg\.value\s*(?:<=|<)\s*2300\))[\s\S]*?\bsend\b'
     # }
-    
-    #block number denpendency
+    #timestamp dependency
+    vulnerability_patterns = {
+    'reliance_on_block_timestamp': r'block\.timestamp',
+    'miner_manipulation_risks': r'block\.timestamp|block\.number',
+    'incorrect_time_estimation': r'block\.number\s*(?:\*|\/)\s*\d+',
+    'timestamp_used_for_randomness': r'block\.timestamp.*%(?:\s*\d+)'
+    }
+    # block number denpendency
     # vulnerability_patterns = {
     # 'dependence_on_block_number': r'block\.number',
     # 'miner_manipulation_risks': r'block\.(timestamp|number)',
     # 'usage_in_comparison_operations': r'block\.number\s*(==|!=|>|<|>=|<=)\s*<number>',
     # 'random_number_generation': r'(block\.blockhash\(block\.number\)|block\.number)'
     # }
+    
+    
     issues = []
     for issue, pattern in vulnerability_patterns.items():
         if re.search(pattern, function_content, re.MULTILINE | re.DOTALL):
@@ -69,7 +69,6 @@ def extract_functions_with_issues(content):
     functions_with_issues = []
     # Enhanced regex pattern for Solidity functions
     function_pattern = re.compile(
-        r'function\s+\w+\s*\([^)]*\)\s*(?:public|private|internal|external)?\s*(?:pure|view|payable)?\s*(?:returns\s*\(.*?\))?\s*\{[\s\S]*?\}',
         r'function\s+\w+\s*\([^)]*\)\s*(?:public|private|internal|external)?\s*(?:pure|view|payable)?\s*(?:returns\s*\(.*?\))?\s*\{[\s\S]*?\}',
         re.MULTILINE | re.DOTALL
     )
